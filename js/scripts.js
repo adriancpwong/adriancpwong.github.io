f5c3cfd5a0f214eae7dd453dc207c180095d4049
adriancpwong <adriancpwong@hotmail.com>
2021-01-02 18:01:33 +0000

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Nav shrink code
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -6,4 +8,21 @@ function scrollFunction() {
   } else {
     document.getElementById("nav-section").style.padding = "20px 0px";
   }
+}
+
+//Collapsable skills code
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
